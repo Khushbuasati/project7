2a162c43f36c195b37f1fb6b01ffa377d1d022b7
Khushbu Asati <khushbu@purecodesoftware.com>
2023-05-08 12:06:25 +0530

    Design in progress

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,17 +1,5 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import React from 'react';
+import { Box, CssBaseline, Toolbar } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
 import AppBarRoot from './appBar';
@@ -19,8 +7,6 @@ import DrawerRoot from './drawer';
 import SmallCards from './smallCard';
 import ProgressCards from './progressCard';
 import InfoCards from './infoCard';
-import Title from '../UIComponents/title';
-
 
 const drawerWidth = 200;
 
